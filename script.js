99a85e2be9d79043e4c49292945ceff858236b7b
Gonzague de Poulpiquet <gdepoulpiquet@me.com>
2022-02-14 16:31:52 +0100

    rajoute un hardmode

--- script.js
+++ script.js
@@ -7,11 +7,14 @@ const keyboard = document.querySelector("[data-keyboard]");
 const alertContainer = document.querySelector("[data-alert-container]");
 const guessGrid = document.querySelector("[data-guess-grid]");
 const langue = document.querySelector("#lang");
+const hard =document.querySelector("#hardmode");
+console.dir(hard)
 /* gestion de la date du jour */
 let dateA = new Date();
 const dateAujourdhui = () => {
   return `${dateA.getDate()}/${dateA.getMonth() + 1}/${dateA.getFullYear()}`;
 };
+const lettresOK = [];
 const resultat = {
   date: dateAujourdhui(),
   numero: 0,
@@ -37,10 +40,12 @@ langue.addEventListener("change", () => {
   localStorage.setItem("langue", JSON.stringify(lang));
   window.location.reload();
 });
-
-
+let hardm= hard.value;
+hard.addEventListener("change",()=>{
+  hardm=hard.value
+})
 startInteraction();
-
+/* ---------------- fonctions---------------- */
 function startInteraction() {
   document.addEventListener("click", handleMouseClick);
   document.addEventListener("keydown", handleKeyPress);
@@ -102,12 +107,22 @@ function deleteKey() {
   delete lastTile.dataset.state; /* supprime les 2 dataset */
   delete lastTile.dataset.letter;
 }
+function rajoutLettre(lettre) {
+  if (!lettresOK.find((l) => l === lettre)) lettresOK.push(lettre);
+}
+function manqueLettres(guess) {
+  let renvoi = false;
+  lettresOK.forEach((ll) => {
+    if (!guess.includes(ll)) renvoi = true;
+  });
+  return renvoi;
+}
 
 function submitGuess() {
   const activeTiles = [...getActiveTiles()];
 
   if (activeTiles.length !== WORD_LENGTH) {
-    showAlert(`${activeTiles.length} lettres ?`,"erreur",1000);
+    showAlert(`${activeTiles.length} lettres ?`, "erreur", 1000);
     shakeTiles(activeTiles);
     return;
   }
@@ -116,11 +131,17 @@ function submitGuess() {
     return word + tile.dataset.letter;
   }, "");
   if (!motExist(guess, lang)) {
-    showAlert("Mot inconnu !","erreur",1000 );
+    showAlert("Mot inconnu !", "erreur", 1000);
     shakeTiles(activeTiles);
     return;
   }
+  console.log(hardm)
 
+  if(manqueLettres(guess)&&(hardm==="h")) {
+    showAlert("manque lettres", "erreur",1000)
+    shakeTiles(activeTiles);
+    return
+  }
   stopInteraction();
 
   activeTiles.forEach((...params) => flipTile(...params, guess, activeTiles));
@@ -142,9 +163,11 @@ function flipTile(tile, index, array, guess, activeTiles) {
       if (targetWord[index] === letter) {
         tile.dataset.state = "correct";
         key.classList.add("correct");
+        rajoutLettre(letter);
       } else if (targetWord.includes(letter)) {
         tile.dataset.state = "pas-la";
         key.classList.add("pas-la");
+        rajoutLettre(letter);
       } else {
         tile.dataset.state = "faux";
         key.classList.add("faux");
@@ -177,8 +200,8 @@ function flipTile(tile, index, array, guess, activeTiles) {
             resultat.target = targetWord;
             resultat.langue = lang;
             let dateJeuxLast = "";
-            if (jeux.length>0) {
-              console.log(jeux)
+            if (jeux.length > 0) {
+              console.log(jeux);
               dateJeuxLast = jeux[jeux.length - 1].date;
             }
             if (resultat.date == dateJeuxLast) {
@@ -199,20 +222,23 @@ const getActiveTiles = () => {
   return guessGrid.querySelectorAll('[data-state="active"]');
 };
 
-
 function showAlert(message, why, duration = 1000) {
   const alert = document.createElement("div");
   alert.textContent = message;
   alert.classList.add("alert");
-  alert.classList.add(why)
-  alertContainer.append(alert);/* au lien de prepend */
+  alert.classList.add(why);
+  alertContainer.append(alert); /* au lien de prepend */
   if (duration == null) return;
 
   setTimeout(() => {
     alert.classList.add("hide");
-    alert.addEventListener("transitionend", () => {
-      alert.remove();
-    },{once:true});
+    alert.addEventListener(
+      "transitionend",
+      () => {
+        alert.remove();
+      },
+      { once: true }
+    );
   }, duration);
 }
 
@@ -231,23 +257,27 @@ function shakeTiles(tiles) {
 
 function checkWinLose(guess, tiles) {
   if (guess === targetWord) {
-    showAlert("Bravo!","bravo", 5000);
+    showAlert("Bravo!", "bravo", 5000);
     danceTiles(tiles);
     console.log(resultat);
     resultat.reussi = true;
     jeux.push(resultat);
     localStorage.setItem("jeux", JSON.stringify(jeux));
-    stopInteraction()
+    stopInteraction();
     return;
   }
 
   const remainingTiles = guessGrid.querySelectorAll(":not([data-letter])");
   if (remainingTiles.length === 0) {
-    showAlert("Désolé.. C'était " + targetWord.toUpperCase() + " !","erreur", null);
+    showAlert(
+      "Désolé.. C'était " + targetWord.toUpperCase() + " !",
+      "erreur",
+      null
+    );
     resultat.reussi = false;
     jeux.push(resultat);
     localStorage.setItem("jeux", JSON.stringify(jeux));
-    stopInteraction()
+    stopInteraction();
   }
 }
 
